6eb0f2caf0666236476070fe4eff73dc8b0fda91
lha5 <hyeahnlee5@gmail.com>
2020-10-02 12:42:51 +0900

    비밀 설정 정보 관리 중

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-mongoose.connect('mongodb+srv://lha5:rgdd0207gg@boilerplate.f1r3z.mongodb.net/boilerplate?retryWrites=true&w=majority', {
+const config = require('./config/key');
+
+const {User} = require('./model/User');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
+
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -14,6 +23,20 @@ mongoose.connect('mongodb+srv://lha5:rgdd0207gg@boilerplate.f1r3z.mongodb.net/bo
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.post('/register', (req, res) => {
+    // 회원 가입할 때 필요한 정보들을 클라이언트에서 가져오면 그것들을 데이터베이스에 넣어준다.
+    const user = new User(req.body);
+
+    user.save((err, userInfo) => {
+        if (err) {
+            return res.json({success: false, err})
+        }
+        return res.status(200).json({
+            success: true
+        })
+    });
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
