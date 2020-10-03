fc5b571f2066c519d151e494f9da7bf107c7cb17
lha5 <hyeahnlee5@gmail.com>
2020-10-03 14:16:07 +0900

    로그아웃 기능 추가

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -2,11 +2,22 @@ import React, {useEffect} from 'react';
 import axios from 'axios';
 import {Box, Heading, Main} from 'grommet';
 
-function LandingPage() {
+function LandingPage(props) {
 
     useEffect(() => {
         axios.get('/api/hello').then(response => console.log(response.data));
     }, []);
+
+    const onClickHandler = () => {
+        axios.get('/api/user/logout')
+            .then(response => {
+                if (response.data.success) {
+                    props.history.push('/login');
+                } else {
+                    alert('로그아웃을 할 수 없습니다.');
+                }
+            });
+    };
 
     return (
         <Main>
@@ -15,6 +26,7 @@ function LandingPage() {
                 justify="center"
             >
                 <Heading>시작 페이지</Heading>
+                <button onClick={onClickHandler}>Logout</button>
             </Box>
         </Main>
     );
